c9024efbd82d38f26d8813d26221cfbfc15386bc
Olha Purtova <olya.shalashova@gmail.com>
2025-01-24 10:06:36 +0100

    fixed tailwind issue

--- src/Components/Account/Communication/Form/AccountForm.php
+++ src/Components/Account/Communication/Form/AccountForm.php
@@ -3,7 +3,7 @@
 namespace App\Components\Account\Communication\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type as FormType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -13,8 +13,9 @@ class AccountForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('amount', MoneyType::class, [
+            ->add('amount', FormType\MoneyType::class, [
                 'label' => 'Amount',
+                'currency' => false,
                 'attr' => [
                     'required' => true,
                     'class' => 'border border-gray-300 rounded-lg px-4 py-2 text-green-800 font-normal focus:outline-none focus:ring-2 focus:ring-green-400',
